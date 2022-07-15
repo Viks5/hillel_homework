86f08353634e97531f263a0a5b5c4f5c592e8c34
Viktoriia <vduru87@gmail.com>
2022-07-15 22:13:48 +0300

    second commit

--- src/main/java/com/hillel/klysenko/lesson6/Homework7.java
+++ src/main/java/com/hillel/klysenko/lesson6/Homework7.java
@@ -1,2 +1,25 @@
-package com.hillel.klysenko.lesson6;public class Homework7 {
+package com.hillel.klysenko.lesson6;
+
+public class Homework7 {
+    public static void main(String[] args) {
+        int sequenceNumber = 1;
+        int shatlleNumber = 1;
+        while (sequenceNumber <= 100) {
+            if (check(shatlleNumber)) {
+                System.out.println("Sequence number is: " + sequenceNumber +
+                        ", shattle number is: " + shatlleNumber);
+                sequenceNumber++;
+            }
+            shatlleNumber++;
+        }
+    }
+        static boolean check(int unluckyNumber) {
+        if ((unluckyNumber == 4) || (unluckyNumber == 9) ||
+                (unluckyNumber % 10 == 4) || (unluckyNumber % 10 == 9) ||
+                ((unluckyNumber % 100) /10 == 4) || ((unluckyNumber % 100) /10 == 9)) {
+            return false;
+            } else {
+            return true;
+        }
+      }
 }
