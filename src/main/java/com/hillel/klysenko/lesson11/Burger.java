8a43030e3ad598dc4f9575eecbd18978d1274ad5
Viktoriia <vduru87@gmail.com>
2022-07-25 20:34:31 +0300

    second commit add number of piece of meats for Burger with double meat

--- src/main/java/com/hillel/klysenko/lesson11/Burger.java
+++ src/main/java/com/hillel/klysenko/lesson11/Burger.java
@@ -10,16 +10,31 @@ public class Burger {
     String mayonnaise;
     public Burger(String bup, String meat,
                   String cheese, String greens, String mayonnaise) {
+        this.bup = bup;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
             System.out.println("Standard burger consist of: " + bup + ", " + meat + ", "
                 + cheese + ", " + greens + ", " + mayonnaise);
     }
     public Burger(String bup, String meat,
                   String cheese, String greens) {
+        this.bup = bup;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
         System.out.println("Dietary burger consist of: " + bup + ", " + meat + ", "
                 + cheese + ", " + greens +  " (without mayonnaise)");
     }
     public Burger(String bup, String numberOfMeat, String meat,
                   String cheese, String greens, String mayonnaise) {
+        this.bup = bup;
+        this.numberOfMeat = numberOfMeat;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
         System.out.println("Burger with double meat consist of: " + bup + ", " + numberOfMeat + " " + meat + ", "
                 + cheese + ", " + greens +  ", " + mayonnaise);
     }
